8fb0ef294ba952b9dd1e3ca3ad0e56fee2da9f42
Ogbai1 <98412628+Ogbai1@users.noreply.github.com>
2022-02-08 08:06:14 +0000

    Add files via upload

---
+++ Session-4 task1.py
@@ -0,0 +1,10 @@
+Numbers= open("star.txt","w")
+Numbers.write('3','45','83','21')
+Numbers.read(4)
+Numbers.close()
+
+Numbers=open("star.text,""r")
+Numbers.read(4)
+for i in a_list:
+   textfile.write(i, + "\n")
+textfile.close()
