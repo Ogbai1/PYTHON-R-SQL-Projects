d2566c9eebe549c028ce7569e71b2068b4942f75
Ogbai1 <98412628+Ogbai1@users.noreply.github.com>
2022-03-20 13:44:51 +0000

    Add files via upload

---
+++ SQL part 1.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE TTA;
+USE TTA;
+CREATE TABLE PACOURSE(Primarykey CHAR(10),
+                    Course_year CHAR,
+				    Course_category CHAR,
+                   no_of_classes INT,
+                   Course_Code DOUBLE);
+SELECT*FROM PACOURSE;
+ 
+
+
+
+
+
